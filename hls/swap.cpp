1c0655f3c7eb76f6da05e29e8c7d2b9b10e4e8d1
Yuichi Sugiyama <sugiyama98i@gmail.com>
2020-07-17 14:34:50 +0900

    Fix swap functions. Close #1

--- hls/swap.cpp
+++ hls/swap.cpp
@@ -3,12 +3,16 @@
 
 void swap1(ap_int<DATA_W> &data1, ap_int<DATA_W> &data2) {
 	if (data1 < data2) {
-		std::swap(data1, data2);
+		ap_int<DATA_W> tmp = data2;
+		data2 = data1;
+		data1 = tmp;
 	}
 }
 
 void swap2(ap_int<DATA_W> &data1, ap_int<DATA_W> &data2) {
 	if (data1 > data2) {
-		std::swap(data1, data2);
+		ap_int<DATA_W> tmp = data2;
+		data2 = data1;
+		data1 = tmp;
 	}
 }
